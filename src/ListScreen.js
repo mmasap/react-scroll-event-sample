5f948d0c36b72c93f710fede2f2f5f2988dd8698
三浦政大 <miura.worker@gmail.com>
2020-12-16 15:51:39 +0900

    update sample source

--- src/ListScreen.js
+++ src/ListScreen.js
@@ -3,18 +3,20 @@ import React, { useEffect } from 'react';
 const ListScreen = (props) => {
   useEffect(() => {
     let timer;
-    window.addEventListener('scroll', () => {
+    const scrollEvent = () => {
       clearTimeout(timer);
       timer = setTimeout(() => {
         console.log('scroll event');
       }, 500);
-    });
+    };
+    window.addEventListener('scroll', scrollEvent);
+    return () => window.removeEventListener('scroll', scrollEvent);
   }, []);
 
   return (
     <div style={{ height: 1000 }}>
       <p>ListScreen</p>
-      <button onClick={props.clickConfirm}>Confirm</button>
+      <button onClick={props.clickConfirm}>Detail</button>
     </div>
   );
 };
